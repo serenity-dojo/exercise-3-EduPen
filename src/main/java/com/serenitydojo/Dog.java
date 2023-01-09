18c6951b43c1d6fae3351f2cef41225153c7870f
ZEKERIYA BUYUKGOZE <65710973+EduPen@users.noreply.github.com>
2023-01-09 17:11:43 +0100

    Create Dog.java
    getter , constructors and variables added

---
+++ src/main/java/com/serenitydojo/Dog.java
@@ -0,0 +1,28 @@
+package comserenitydojo;
+
+public class Cat{
+
+private String  name ;
+private String favoriteToy;
+private int age ;
+
+public void Cat{
+     this.name =name;
+  this.favoriteToy =favoriteToy;
+  this.age=age;
+}
+  
+  public String getName(){
+  this.name =name;
+  }
+  
+  public String getFavoriteToy(){
+  this.favoriteToy =favoriteToy;
+  }
+  
+    public String getAge(){
+  this.age =age;
+  }
+
+
+}
